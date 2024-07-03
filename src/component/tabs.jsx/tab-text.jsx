08205045248c1fa1061e002455a9b0d47a51c4f4
Umer qazi <uqazi844@gmail.com>
2024-07-03 17:46:44 +0500

    commit

---
+++ src/component/tabs.jsx/tab-text.jsx
@@ -0,0 +1,38 @@
+import Tabs from "./tabs";
+import './style.css'
+
+
+export default function TabTest () {
+
+
+    function RandomComponent () {
+        return (
+            <h1>Some Random Content</h1>
+        )
+    }
+
+    function handleChange (currentTabIndex) {
+        console.log(currentTabIndex)
+    }
+
+    const tabs = [
+        {
+            label: "Tab 1",
+            content: <div>This is content for Tab1</div>
+        },
+        {
+            label: "Tab 2",
+            content: <div>This is content for Tab2</div>
+        },
+        {
+            label: "Tab 3",
+            content: <RandomComponent />
+        },
+
+    
+
+    ]
+    return (
+        <Tabs tabsContent = {tabs} onChange={handleChange}/>
+    )
+}
